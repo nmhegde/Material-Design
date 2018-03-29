988da64ae65859c86ac3c7c191912784c6642de2
Nikhil Hegde <nikhilmhegde@gmail.com>
2018-03-29 13:20:54 -0400

    initial

---
+++ 1.js
@@ -0,0 +1,4 @@
+M.AutoInit();
+var elem = document.querySelector('.carousel');
+
+  var instance = M.Carousel.init(elem, options);
---
+++ js/1.js
@@ -0,0 +1,14 @@
+
+//Initialize
+M.AutoInit();
+
+//Carousel
+var elem = document.querySelector('.carousel');
+var instance = M.Carousel.init(elem, options);
+
+//Dropdown
+var elem = document.querySelector('.dropdown-trigger');
+  var instance = M.Dropdown.init(elem, options);
+
+  
+
